805f90c449b3049cd71bc90b9f500a6697851522
Elbehary <ahmedelbehary0@gmail.com>
2019-06-19 17:55:55 +0200

    post and delete

--- web.js
+++ web.js
@@ -18,13 +18,39 @@ router.get("/api/user/:userId", (req, res) => {
     });
 });
 router.post("/api/user/:userId/avatar", (req, res) => {
+  console.log(req.file);
   let avatar;
   let id = req.params.userId;
+  let fileInfo = [];
+  axios
+    .get(`https://reqres.in/api/users/${id}`)
+    .then(item => {
+      for (let i = 0; i < item.data.data; i++) {
+        fileInfo.push({
+          originalName: req.files[i].originalName,
+          size: req.files[i].size,
+          b64: new Buffer(fs.readFileSync(req.files[i].path)).toString("base64")
+        });
+        fs.unlink(req.files[i].path);
+      }
+      avatar = item.data.data.avatar;
+      res.send({ avatar });
+    })
+    .catch(err => {
+      console.error(err);
+      return err;
+    });
+});
+
+router.delete("/api/user/:userId/avatar", (req, res) => {
+  console.log(req.file);
+  let avatar;
+  let id = req.params.userId;
+  let fileInfo = [];
   axios
     .get(`https://reqres.in/api/users/${id}`)
     .then(item => {
       avatar = item.data.data.avatar;
-      console.log(avatar);
       res.send({ avatar });
     })
     .catch(err => {
